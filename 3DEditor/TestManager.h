8c51ac2ae16159d64418fa0463fcb6cd5853a419
Svilen M. Varbanov <svilen_varbanov@msn.com>
2024-04-11 11:11:52 +0300

    Obsolete code.

--- 3DEditor/TestManager.h
+++ 3DEditor/TestManager.h
@@ -37,14 +37,6 @@ public: // Methods
 	static void FindFiles(const wchar_t* szFolder, const wchar_t* szWildcard, CStringList& lsFiles);
 
 	// --------------------------------------------------------------------------------------------
-	void GenerateTests(const CString& strWildcards);	
-
-	// --------------------------------------------------------------------------------------------
-	void ExecuteTests(const wchar_t* szTestsFolder);	
-
-private: // Methods
-
-	// --------------------------------------------------------------------------------------------
-//	void Test(const wchar_t* szInputFileName, const wchar_t* szTestFileName, const wchar_t* szScreenShotFilePath, CTestHtmlReport* pReport);
+	void GenerateTests(const CString& strWildcards);
 };
 
