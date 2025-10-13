0065f27f111423de6798718150ba527f80e51a6c
I.Sokolov@yandex.ru <I.Sokolov@yandex.ru>
2025-10-13 18:14:37 +0400

    update engine revision 2174

--- DragFace/GeomTypes.h
+++ DragFace/GeomTypes.h
@@ -1,5 +1,19 @@
 #pragma once
 
+#if 0
+mathematicsGeometryDoublePrecision.cpp / .h
+mathematicsGeometryDoublePrecisionDerived.cpp
+//
+//	https://math.stackexchange.com/questions/2213165/find-shortest-distance-between-lines-in-3d
+//
+double	LineLineDistance(
+	const VECTOR3 * lineI_pointI,
+	const VECTOR3 * lineI_pointII,
+	const VECTOR3 * lineII_pointI,
+	const VECTOR3 * lineII_pointII
+)
+#endif
+
 typedef point3d<double> Point3d;
 
 typedef vector3d<double> Vector3d;
--- engine/include/repo.h
+++ engine/include/repo.h
@@ -148,7 +148,7 @@ const char     DECL * STDC  repo_GetPrefixByIterator   (RdfFile file, const char
 //      -- for "<prefix>:<local_name>" the function will return URI value of the prefix concatenated with <local_name>
 //      -- when separator is missed ("<local_name>") it will return <local_name>
 //
-// Return NULL if fails
+// Return "" if fails
 // !!! Returned pointer is valid until next call to the function
 //
 const char     DECL * STDC  repo_ResolveName           (RdfFile file, const char* prefixedOrIriName, char separator);
@@ -161,7 +161,7 @@ const char     DECL * STDC  repo_ResolveName           (RdfFile file, const char
 // 
 // The function will return suitable prefixed name or (when no suitable prefix found) fully qualified IFREF name.
 //
-// Return NULL if fails
+// Return "" if fails
 // !!! Returned pointer is valid until next call to the function
 //
 const char     DECL * STDC  repo_GetPrefixedName        (RdfFile file, const char* iriName, char separator);
