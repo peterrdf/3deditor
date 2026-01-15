057911c8e281665440a1e5648cb4be075632c03a
I.Sokolov@yandex.ru <I.Sokolov@yandex.ru>
2026-01-15 17:45:50 +0700

    update engine

--- engine/include/exchange.h
+++ engine/include/exchange.h
@@ -15,7 +15,7 @@ RdfFile      DECL STDC  exchange_Read            (ParaModelPtr model, const char
 
 //saves file
 //returns 1 or 0 (see paralib_GetLastError if fails)
-int64_t      DECL STDC  exchange_WriteTTL           (RdfFile file, const char* path);
+int64_t      DECL STDC  exchange_WriteTTL           (RdfFile file, const char* path, bool rulesAsTextFormulas);
 
 //
 bool         DECL STDC  exchange_ImportRdfBin       (RdfFile file, const char* path);
