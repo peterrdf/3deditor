d8da4c5e9d4b8d112c57b0bb94278b59b2d48062
svilenvarbanov2019 <52557863+svilenvarbanov2019@users.noreply.github.com>
2026-09-16 09:22:56 +0300

    SDK.

--- include/_ifc_model.cpp
+++ include/_ifc_model.cpp
@@ -9,9 +9,6 @@
 #include <cfloat>
 
 // ************************************************************************************************
-#define DEFAULT_CIRCLE_SEGMENTS 36
-
-// ************************************************************************************************
 _ifc_model::_ifc_model(_log* pLog, bool bUseWorldCoordinates /*= false*/, bool bLoadInstancesOnDemand /*= false*/)
 	: _ap_model(pLog, enumAP::IFC)
 	, m_bUseWorldCoordinates(bUseWorldCoordinates)
@@ -150,9 +147,9 @@ void _ifc_model::loadInstances(bool bClean /*= true*/)
 	} // if (pEntityProvider != nullptr)
 	progressInit(iTotal, "Loading instances");
 
-	retrieveGeometryRecursively(m_sdaiObjectEntity, DEFAULT_CIRCLE_SEGMENTS);
-	retrieveGeometry("IFCPROJECT", DEFAULT_CIRCLE_SEGMENTS);
-	retrieveGeometry("IFCRELSPACEBOUNDARY", DEFAULT_CIRCLE_SEGMENTS);
+	retrieveGeometryRecursively(m_sdaiObjectEntity, DEFAULT_SEGMENTATION_PARTS);
+	retrieveGeometry("IFCPROJECT", DEFAULT_SEGMENTATION_PARTS);
+	retrieveGeometry("IFCRELSPACEBOUNDARY", DEFAULT_SEGMENTATION_PARTS);
 
 	if (getMultiThreadedLoad()) {
 		unsigned int threadsCount = thread::hardware_concurrency() / 4;
@@ -370,6 +367,9 @@ void _ifc_model::loadInstances(bool bClean /*= true*/)
 			}
 		} // else if ((arOffset[0] + arOffset[1] + arOffset[2]) != 0.)
 	} // if (!m_vecMappedItemPendingUpdate.empty())
+
+	// Progress
+	progressEnd();
 
 	scale();
 
@@ -672,6 +672,7 @@ OwlInstance _ifc_model::createMapConversionTransformation()
 {
 	_ap_model::addGeometry(pGeometry);
 
+	// Progress
 	progressStep();
 }
 
@@ -1063,7 +1064,7 @@ _geometry* _ifc_model::loadGeometry(SdaiInstance sdaiInstance, bool bMappedItem,
 	}
 
 	// Set up segmentation
-	if (iCircleSegments != DEFAULT_CIRCLE_SEGMENTS) {
+	if (iCircleSegments != DEFAULT_SEGMENTATION_PARTS) {
 		setSegmentation(getSdaiModel(), iCircleSegments, 5);
 	}
 
@@ -1083,8 +1084,8 @@ _geometry* _ifc_model::loadGeometry(SdaiInstance sdaiInstance, bool bMappedItem,
 	}
 
 	// Restore segmentation
-	if (iCircleSegments != DEFAULT_CIRCLE_SEGMENTS) {
-		setSegmentation(getSdaiModel(), DEFAULT_CIRCLE_SEGMENTS, 5);
+	if (iCircleSegments != DEFAULT_SEGMENTATION_PARTS) {
+		setSegmentation(getSdaiModel(), DEFAULT_SEGMENTATION_PARTS, 5);
 	}
 
 	return pGeometry;
@@ -1093,7 +1094,7 @@ _geometry* _ifc_model::loadGeometry(SdaiInstance sdaiInstance, bool bMappedItem,
 _geometry* _ifc_model::loadGeometry(const IFC_GEOMETRY& ifcGeometry, MultiThreadOwlModelWrapper multiThreadOwlModelWrapper)
 {
 	// Set up segmentation
-	if (ifcGeometry.iCircleSegments != DEFAULT_CIRCLE_SEGMENTS) {
+	if (ifcGeometry.iCircleSegments != DEFAULT_SEGMENTATION_PARTS) {
 		setSegmentation(multiThreadOwlModelWrapper, ifcGeometry.iCircleSegments, 5);
 	}
 
@@ -1116,8 +1117,8 @@ _geometry* _ifc_model::loadGeometry(const IFC_GEOMETRY& ifcGeometry, MultiThread
 	}
 
 	// Restore segmentation
-	if (ifcGeometry.iCircleSegments != DEFAULT_CIRCLE_SEGMENTS) {
-		setSegmentation(multiThreadOwlModelWrapper, DEFAULT_CIRCLE_SEGMENTS, 5);
+	if (ifcGeometry.iCircleSegments != DEFAULT_SEGMENTATION_PARTS) {
+		setSegmentation(multiThreadOwlModelWrapper, DEFAULT_SEGMENTATION_PARTS, 5);
 	}
 
 	return pGeometry;
